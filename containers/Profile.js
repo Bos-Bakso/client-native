4d7c89501e097bf06b90535d4ea789f8d2dce828
Ayu Sudi <ayusudi.abc@gmail.com>
2019-11-16 18:00:21 +0700

    service and half rank

--- containers/Profile.js
+++ containers/Profile.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import {View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import {View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity,ScrollView} from 'react-native'
 import {useDispatch} from 'react-redux'
 import {logoutMe} from '../redux/actions/postLogin'
 import styleg from '../styleGlobal'
-
+import axios from 'axios'
 
 export default function Ranking(props){
+  const [data, setData] = useState([])
   const dispatch = useDispatch()
   const logoutNow = () => {
     dispatch(logoutMe())
     props.navigation.navigate("Login")
   }
+
+  // useEffect(() => {
+    // axios.get("http://34.87.107.88/user")
+    // .then(({data}) => {
+    //   data.sort((a, b) => b)
+    //   console.log(data);
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // })
+  
+  // }, [])
+
   return (
     <View style={styleg.safearea}>
+      <ScrollView>
            <TouchableOpacity onPress={() => logoutNow()}>
                 <Text>Logout</Text>
               </TouchableOpacity>
           <Text>Ranking</Text>
+      </ScrollView>
     </View>
   )
 }
