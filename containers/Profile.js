b898733a41c491df01988be44735771198d19f0f
Ayu Sudi <ayusudi.abc@gmail.com>
2019-11-19 09:27:37 +0700

    loading

--- containers/Profile.js
+++ containers/Profile.js
@@ -8,12 +8,10 @@ import axios from 'axios'
 import yellow from '../assets/yellow.png'
 import cube from '../assets/wallpapers/cube.jpg'
 import db from '../config/firebase'
-
+// import Rotate from '../components/Rotate'
 import LineRank from '../components/LineRank'
 import award from '../assets/icons/ribbon.png'
 import bgLog from '../assets/wal/wal.jpg'
-
-
 
 export default function Ranking(props) {
   const dispatch = useDispatch()
@@ -21,14 +19,14 @@ export default function Ranking(props) {
   const image = useSelector(state => state.loginAcc.image) || "https://storage.googleapis.com/bosbakso/1573807677370kachu.jpg"
   const username = useSelector(state => state.loginAcc.username) || "username"
   const data = useSelector(state => state.rankBakso.ranks)
-  const totalBowl = useSelector(state => state.rankBakso.totalBowl) 
-  const totalMoney = useSelector(state => state.rankBakso.totalMoney) 
+  const totalBowl = useSelector(state => state.rankBakso.totalBowl)
+  const totalMoney = useSelector(state => state.rankBakso.totalMoney)
   const myRank = useSelector(state => state.rankBakso.myRank)
 
   useEffect(() => {
-    db.collection('triggerRank').onSnapshot(function(querySnapshot) {
-      dispatch(getRank(_id)) 
-  });
+    db.collection('triggerRank').onSnapshot(function (querySnapshot) {
+      dispatch(getRank(_id))
+    });
   }, [])
 
 
@@ -37,44 +35,48 @@ export default function Ranking(props) {
     props.navigation.navigate("Login")
   }
 
-  if (data){
+  if (data) {
     return (
       <View style={{ height: '100%', ...styleg.safearea }}>
 
 
-      <ScrollView>
-        <View style={{ width: '100%', height: '100%', ...styleg.safearea, justifyContent: 'flex-start', alignItems: 'center', flex: 1 }}>
-          <View style={{ width: '90%', height: 250, backgroundColor: 'whitesmoke', justifyContent: 'center', alignItems: 'center', paddingTop: 5, marginVertical: 5 }}>
+        <ScrollView>
+          <View style={{ width: '100%', height: '100%', ...styleg.safearea, justifyContent: 'flex-start', alignItems: 'center', flex: 1 }}>
+            <View style={{ width: '90%', height: 250, backgroundColor: 'whitesmoke', justifyContent: 'center', alignItems: 'center', paddingTop: 5, marginVertical: 5 }}>
 
-            <ImageBackground source={award} style={{ width: 65, height: 65, alignSelf: 'flex-end', justifyContent: 'flex-start', alignItems: 'center', marginRight: 5 }}>
-              <Text style={{ fontSize: 16, marginTop: 12 }}>#{myRank}</Text>
-            </ImageBackground>
-            <Image source={{ uri: image }} style={{ width: 115, height: 115, borderRadius: 115 / 2, marginBottom: 6, marginTop: -30 }} />
-            <Text style={{ fontSize: 20, fontWeight: '600' }}>{username}</Text>
-            <Text style={{ fontSize: 15 }}>{totalBowl} Bowl</Text>
-            <Text style={{ fontSize: 12 }}>{totalMoney}</Text>
+              <ImageBackground source={award} style={{ width: 65, height: 65, alignSelf: 'flex-end', justifyContent: 'flex-start', alignItems: 'center', marginRight: 5 }}>
+                <Text style={{ fontSize: 16, marginTop: 12 }}>#{myRank}</Text>
+              </ImageBackground>
+              <Image source={{ uri: image }} style={{ width: 115, height: 115, borderRadius: 115 / 2, marginBottom: 6, marginTop: -30 }} />
+              <Text style={{ fontSize: 20, fontWeight: '600' }}>{username}</Text>
+              <Text style={{ fontSize: 15 }}>{totalBowl} Bowl</Text>
+              <Text style={{ fontSize: 12 }}>{totalMoney}</Text>
 
-            <TouchableOpacity onPress={() => logoutNow()} style={{ marginTop: 8, width: '100%', height: 30, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ color: 'white', textAlign: 'center', fontWeight: '600' }}>Logout</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{ width: '100%', alignItems: 'center', justifyContent: 'flex-start', marginTop: 20, backgroundColor: '#f6f9fcd0', paddingBottom: 20, borderRadius: 10 }}>
-            <View style={{ width: '90%' }}>
-              <Text style={{ textDecorationLine: 'underline', fontSize: 28, fontWeight: 'bold', textAlign: "right", marginTop: 12, marginBottom: 8, alignSelf: 'flex-start' }}>Rank</Text>
+              <TouchableOpacity onPress={() => logoutNow()} style={{ marginTop: 8, width: '100%', height: 30, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: 'white', textAlign: 'center', fontWeight: '600' }}>Logout</Text>
+              </TouchableOpacity>
             </View>
-            {
-              data.map((el, i) =>
-              <View key={i} style={{ flexDirection: 'row', width: '90%', backgroundColor: '#F3F4F5', height: 100, marginVertical: 8, borderRadius: 10 }}>
-                  <LineRank key={i} rank={i + 1} image={el.image} username={el.username} id={el._id} />
-                </View>
-              )
-            }
+            <View style={{ width: '100%', alignItems: 'center', justifyContent: 'flex-start', marginTop: 20, backgroundColor: '#f6f9fcd0', paddingBottom: 20, borderRadius: 10 }}>
+              <View style={{ width: '90%' }}>
+                <Text style={{ textDecorationLine: 'underline', fontSize: 28, fontWeight: 'bold', textAlign: "right", marginTop: 12, marginBottom: 8, alignSelf: 'flex-start' }}>Rank</Text>
+              </View>
+              {
+                data.map((el, i) =>
+                  <View key={i} style={{ flexDirection: 'row', width: '90%', backgroundColor: '#F3F4F5', height: 100, marginVertical: 8, borderRadius: 10 }}>
+                    <LineRank key={i} rank={i + 1} image={el.image} username={el.username} id={el._id} />
+                  </View>
+                )
+              }
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </View>
-  )
-} else {
-  return <></>
-}
+        </ScrollView>
+      </View>
+    )
+  } else {
+    return (
+      <View style={{ ...styleg.safearea, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+
+
+      </View>)
+  }
 }
--- containers/Triall.js
+++ containers/Triall.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Animated, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Animated, Button, TouchableOpacity , Image} from 'react-native';
+import icon from '../assets/icons/ban.png'
 
 export default class App extends Component {
-    constructor() {
+    constructor(props) {
         super()
         this.animated = new Animated.Value(0);
 
-        var range = 1, snapshot = 50, radius = 100;
+        var range = 1, snapshot = 80, radius = 100;
         /// translateX
         var inputRange = [], outputRange = [];
         for (var i=0; i<=snapshot; ++i) {
@@ -32,23 +33,24 @@ export default class App extends Component {
         this.animated.setValue(0)
         Animated.timing(this.animated, {
           toValue: 1,
-          duration: 1000,
+          duration: 2000,
         }).start();
       }
 
-
       render() {
+        this.animate() 
         const transform = [{ translateY: this.translateY }, {translateX: this.translateX}];
         return (
           <View style={styles.container}>
-            <Animated.View style={[{ transform }]}>
-              <TouchableOpacity style={styles.btn}>
-                <Text>hallo</Text>
-              </TouchableOpacity>
+            <Animated.View style={[{ transform }]} >
+            <View style={{backgroundColor: 'whitesmoke', width:60, height: 60, borderRadius: 30, justifyContent: 'center', alignItems: 'center'}}>
+              <Image source={icon} style={{width: 50, height: 50}} />
+            </View>
+            
             </Animated.View>
-            <Button title="Test" onPress={() => { 
-              this.animate() 
-              }} />
+            <View style={{backgroundColor: 'whitesmoke', width:100, height: 100, borderRadius: 50, justifyContent: 'center', alignItems: 'center'}}>
+              <Image source={icon} style={{width: 80, height: 0}} />
+            </View>
           </View>
         );
       }
